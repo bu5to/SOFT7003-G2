bd8f2618a603e1ab074dc8b20f809f97ace0554d
Jorge El Busto <33113637+bu5to@users.noreply.github.com>
2022-02-08 17:10:32 +0000

    render_template commented

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello, World!</p>" #This would be afterwards  replaced by an HTML file.
+    return render_template('index.html') #now, when entering 127.0.0.0:5000/, the file index.html will pop up in the browser.
+
 
 #This is a test comment.
