d4e777b9a489585c6503259f0690aa9ecfea8fc0
Jorge El Busto <33113637+bu5to@users.noreply.github.com>
2022-02-16 15:49:47 +0000

    Split the template into different .html files

--- app.py
+++ app.py
@@ -6,5 +6,33 @@ app = Flask(__name__)
 def index():
     return render_template('index.html') #now, when entering 127.0.0.0:5000/, the file index.html will pop up in the browser.
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
-#This is a test comment.
+@app.route("/services")
+def services():
+    return render_template('services.html')
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route("/team")
+def team():
+    return render_template('team.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+#These URLs above will be later on substituted by the sections specified
+#in the document.
+
+
+
+
+
+
+
+            #This is a test comment.
