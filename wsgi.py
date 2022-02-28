7f5ce317f4db945e9051a5a858afc2c264c6c940
Jorge El Busto <33113637+bu5to@users.noreply.github.com>
2022-02-28 20:32:10 +0000

    Trying to get the form working

--- wsgi.py
+++ wsgi.py
@@ -8,64 +8,78 @@ import click
 from models import User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jiyerqtkdtzmiq:b0b8a7677c2904fe4e37412e1452190cc4368512c9ad0542071835b7ac713ec3@ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/dbh08b84org3rp'
-#Edit to connect to SQL database
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'postgres://jiyerqtkdtzmiq:b0b8a7677c2904fe4e37412e1452190cc4368512c9ad0542071835b7ac713ec3@ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/dbh08b84org3rp'
+# Edit to connect to SQL database
 app.config['SECRET_KEY'] = 'thisisasecretkey'
-#We need to produce an actual secret key, min 30 random characters, min 256 bits.
+# We need to produce an actual secret key, min 30 random characters, min 256 bits.
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
-#This allows flask to manage the login process, i.e. loading users according to IDs.
+
+
+# This allows flask to manage the login process, i.e. loading users according to IDs.
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 @app.route("/")
 def index():
-    return render_template('index.html') #now, when entering 127.0.0.0:5000/, the file index.html will pop up in the browser.
+    return render_template(
+        'index.html')  # now, when entering 127.0.0.0:5000/, the file index.html will pop up in the browser.
+
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
+
 @app.route("/services")
 def services():
     return render_template('services.html')
+
 
 @app.route("/portfolio")
 def portfolio():
     return render_template('portfolio.html')
 
+
 @app.route("/team")
 def team():
     return render_template('team.html')
+
 
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
 
+
 @app.route('/sw.js')
 def sw():
-    response=make_response(
-                     send_from_directory('static',path='sw.js'))
-    #change the content header file. Can also omit; flask will handle correctly.
+    response = make_response(
+        send_from_directory('static', path='sw.js'))
+    # change the content header file. Can also omit; flask will handle correctly.
     response.headers['Content-Type'] = 'application/javascript'
     return response
-#These URLs above will be later on substituted by the sections specified
-#in the document.
+
+
+# These URLs above will be later on substituted by the sections specified
+# in the document.
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-  form = LoginForm()
-  if form.validate_on_submit():
-      user = User.query.filter_by(username=form.username.data).first()
-      if user:
-          if bcrypt.check_password_hash(user.password, form.password.data):
-              login_user(user)
-              return redirect(url_for('index'))
-  return render_template('login.html', form = form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.data['submit']:
+            print("It works")
+            user = User.query.filter_by(username=form.username.data).first()
+            if user:
+                login_user(user)
+                return redirect(url_for('index'))
+    return render_template('login.html', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -73,12 +87,14 @@ def register():
     form = RegisterForm()
     session = Session()
     if form.validate_on_submit():
+        print("It works")
         if form.data['submit']:
-            new_user = User(username = form.username.data, password = hashed_password, email = form.email.data)
+            new_user = User(username=form.username.data, password=hashed_password, email=form.email.data)
             session.add(new_user)
             session.commit()
             return redirect(url_for('login'))
-    return render_template('/register.html', form = form)
+    return render_template('/register.html', form=form)
+
 
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
@@ -86,8 +102,9 @@ def logout():
     logout_user()
     return redirect(url_for='/login')
 
-#@click.command()
-#@with_appcontext
+
+# @click.command()
+# @with_appcontext
 def create_tables():
     Base.metadata.drop_all(bind=engine, tables=[User.__table__])
     Base.metadata.create_all(engine)
@@ -97,6 +114,7 @@ def create_tables():
     session.commit()
     print("Tables have been created.")
     session.close()
+
 
 if __name__ == '__main__':
     create_tables()
