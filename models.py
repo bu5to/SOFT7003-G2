3b093055be3ce00a43ef9e5a3773f87629ebb704
Jorge El Busto <33113637+bu5to@users.noreply.github.com>
2022-02-28 17:15:29 +0000

    Creating tables in the cloud db

--- models.py
+++ models.py
@@ -1,9 +1,19 @@
-from app import db
+from base import Base, Session
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(20), nullable = False, unique = True)
-    password = db.Column(db.String(80), nullable = False)
-    email = db.Column(db.String(30), nullable = False, unique = True)
+class User(Base, UserMixin):
+    __tablename__ = 'username'
+    id = Column(Integer, primary_key = True)
+    username = Column(String(20), nullable = False, unique = True)
+    password = Column(String(80), nullable = False)
+    email = Column(String(30), nullable = False, unique = True)
+
+    def __init__(self, id, name, email, password):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = generate_password_hash(password)
 #Creates a SQL table for storing user information
--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, make_response, send_from_directory, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-#from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
-#from flask_bcrypt import Bcrypt
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
+from flask_bcrypt import Bcrypt
 from models import User
 
 app = Flask(__name__)
-##app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jiyerqtkdtzmiq:b0b8a7677c2904fe4e37412e1452190cc4368512c9ad0542071835b7ac713ec3@ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/dbh08b84org3rp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jiyerqtkdtzmiq:b0b8a7677c2904fe4e37412e1452190cc4368512c9ad0542071835b7ac713ec3@ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/dbh08b84org3rp'
 #Edit to connect to SQL database
-##app.config['SECRET_KEY'] = 'thisisasecretkey'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
 #We need to produce an actual secret key, min 30 random characters, min 256 bits.
 db = SQLAlchemy(app)
 ##bcrypt = Bcrypt(app)
@@ -18,7 +18,7 @@ db = SQLAlchemy(app)
 
 ##@login_manager.user_loader
 ##def load_user(user_id):
-  ##  return User.query.get(int(user_id))
+##    return User.query.get(int(user_id))
 
 @app.route("/")
 def index():
@@ -70,18 +70,18 @@ def sw():
 ##def register():
     ##form = registerForm()
     ##if form.validate_on_submit():
-        ##hashed_password = bcrypt.generate_password_hash(form.password.data)
-        ##new_user = User(username = form.username.data, password = hashed_password, email = form.email.data)
-        ##db.session.add(new_user)
-        ##      db.session.commit()
-        ##  return redirect(url_for('login'))
-    ##    return render_template('/register.html', form = form)
+    ##    hashed_password = bcrypt.generate_password_hash(form.password.data)
+    ##    new_user = User(username = form.username.data, password = hashed_password, email = form.email.data)
+    ##    db.session.add(new_user)
+    ##    db.session.commit()
+  ##      return redirect(url_for('login'))
+##    return render_template('/register.html', form = form)
 
-@app.route('/logout', methods=['GET', 'POST'])
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for='/login')
+#@app.route('/logout', methods=['GET', 'POST'])
+#@login_required
+#def logout():
+ #   logout_user()
+  #  return redirect(url_for='/login')
 
 if __name__ == '__main__':
     app.run(debug == True)
