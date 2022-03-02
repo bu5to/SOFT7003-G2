2323166f72b8002cbf9c809c6aed025f797f4760
Jorge El Busto <33113637+bu5to@users.noreply.github.com>
2022-03-02 08:44:02 +0000

    change db url due to maintenance

--- base.py
+++ base.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-engine = create_engine('postgresql://jiyerqtkdtzmiq:b0b8a7677c2904fe4e37412e1452190cc4368512c9ad0542071835b7ac713ec3@ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/dbh08b84org3rp')
+
+engine = create_engine('postgres://wfasqssjtdjinv:e74fce4710d23c30872f19923aecab934c9a7cfd6e2ee02cab3ad49d440cf415@ec2-52-214-125-106.eu-west-1.compute.amazonaws.com:5432/d28lqknqnpun53')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
